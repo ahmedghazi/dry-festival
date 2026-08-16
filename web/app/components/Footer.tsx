2524675738b5efbbd556b8fa73e1072eb50828aa
ahmedghazi <atmet.ghazi@gmail.com>
2026-08-16 15:01:25 +0200

    fixed footer

--- web/app/components/Footer.tsx
+++ web/app/components/Footer.tsx
@@ -1,27 +1,29 @@
 import React from "react";
-import { SETTINGS_QUERY_RESULT } from "../sanity-api/types/sanity.types";
 import { PortableText } from "next-sanity";
 import portableTextComponents from "../sanity-api/portableTextComponents";
 import Mailchimp from "./ui/Mailchimp";
+import { SETTINGS_QUERY_RESULT } from "../sanity-api/types/sanity.types";
 
 type Props = {
   settings: SETTINGS_QUERY_RESULT;
 };
 
 const Footer = ({ settings }: Props) => {
-  const { contact, contactText } = settings;
+  // const { contact, contactText } = settings;
   return (
     <footer>
       <div className='inner'>
         <h2 className='md:text-lg'>contact</h2>
-        <div className='text'>
-          <div className='module__text text'>
-            <PortableText
-              value={contactText}
-              components={portableTextComponents}
-            />
+        {settings?.contactText && (
+          <div className='text'>
+            <div className='module__text text'>
+              <PortableText
+                value={settings?.contactText}
+                components={portableTextComponents}
+              />
+            </div>
           </div>
-        </div>
+        )}
         <Mailchimp action='' optin />
       </div>
     </footer>
